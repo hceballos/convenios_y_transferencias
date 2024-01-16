07f3a0eb46d7d6e0eecd6d8f87874ad5a5e6c9ba
John Doe <johndoe@example.com>
2024-01-16 16:46:57 -0300

    Mejora de en calculos en lineas de accion

--- lib/centralizacion/readExcel/readtodasLasFES.py
+++ lib/centralizacion/readExcel/readtodasLasFES.py
@@ -6,14 +6,18 @@ class ReadtodasLasFES:
 	def __init__(self, datos):
 		self.datos = datos
 
-		todosLosPagos = pd.DataFrame()
+		fes = pd.DataFrame()
 		for f in glob.glob('./input_excel/centralizacion/todasLasFES/*.xlsx', recursive=True):
 			print('Procesando  : ', f)
 
-			todosLosPagos_actual = pd.read_excel(f)
-			todosLosPagos = todosLosPagos.append(todosLosPagos_actual, ignore_index=True)
+			fes_actual = pd.read_excel(f)
+			fes = fes.append(fes_actual, ignore_index=True)
 
+		# print(fes.columns)
+		fes['Fechaactualizacion']	= pd.to_datetime(fes['Fechaactualizacion'])
+		fes['ultimo_dia_del_mes'] 	= fes['Fechaactualizacion'] + pd.offsets.MonthEnd(0)
+		fes['nro_dias_Mes'] 		= (fes['ultimo_dia_del_mes'] - fes['Fechaactualizacion']).dt.days
+		fes['Analisis']      		= 'Pendiente'
 
-		#print(todosLosPagos.columns)
-		todosLosPagos['Analisis']      = 'Pendiente'
-
+		database = Database()
+		database.crear_Fes(fes)
